cd47795c21bd147328dd65fbb83164ab7de88332
pgo <pgo@vizrt.com>
2019-02-03 22:27:46 +0200

    Created two scripts to deploy and install the system.

---
+++ install.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+get_abs_filename() {
+  # $1 : relative filename
+  echo "$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"
+}
+
+usage()
+{
+	echo "Usage: $1 -i <input zip file> -o <destination dir>"
+}
+
+while [ "$1" != "" ]; do
+	case $1 in 
+		-i ) 	shift
+				INPUT_FILE=$1
+				;;
+		-o )	shift
+				OUTPUT_DIR=$1
+				;;
+		--help | -h )
+				usage $0
+				exit 1
+				;;
+		* )		usage $0
+				exit 0
+	esac
+	shift
+done
+
+if [ -z "$INPUT_FILE" ] || [ -z "$OUTPUT_DIR" ] 
+then
+	usage $0
+	exit 0
+fi
+
+INPUT_FILE_PATH=$(get_abs_filename $INPUT_FILE)
+OUTPUT_FULL_PATH=$(get_abs_filename $OUTPUT_DIR)
+
+mkdir -p $OUTPUT_FULL_PATH
+cd $OUTPUT_FULL_PATH
+unzip $INPUT_FILE_PATH
+
+
+cd $OUTPUT_FULL_PATH/server
+npm install
+
+cd $OUTPUT_FULL_PATH/client
+npm install
+
+cp -r $OUTPUT_FULL_PATH/client/app/* $OUTPUT_FULL_PATH/server/views
+
+
